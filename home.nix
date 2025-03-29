7d16e67a578b7d39ee394264bc6bfa726eb8d155
kavsdev <kavsdev@proton.me>
2025-03-29 22:01:42 +0530

    first commit

---
+++ home.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ... }:
+
+  let
+    nixgl = import <nixgl> {};
+  in {
+  #Basic stuff
+  home.username = "kavs";
+  home.homeDirectory = "/home/kavs";
+  home.stateVersion = "24.11"; #Dont EVER change
+
+  programs.home-manager.enable = true; #let home-manager manage itself
+  news.display = "silent"; #stops notification popup
+
+  targets.genericLinux.enable = true; #make Home Manager work better on GNU/Linux distributions other than NixOS
+  nixpkgs.config.allowUnfree = true; #allow proprietry packages
+  
+  #import other .nix configs
+  imports = [
+    ./alacritty.nix
+  ];
+
+  #home configs
+  home.packages = with pkgs; [    
+    neofetch
+    verilator
+    nix-info
+    bat
+    quickemu
+    nixgl.nixGLIntel
+    nerd-fonts.fira-code #font
+    #iosevka #font
+    nerd-fonts.iosevka-term #font
+    #alacritty-theme
+    #sanctity #diplay terminal colours
+  ];
+
+
+  #programs.alacritty.package =  config.lib.nixGL.wrap pkgs.alacritty;
+
+  #program configs   
+  programs.fzf = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+
+  #font stuff
+  fonts.fontconfig.enable = true;
+
+}
